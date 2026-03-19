f1f21107a002cafb2f7c4d9275d654b9df1b7c03
bhavya gupta <111831473+bhavyaguptcoding@users.noreply.github.com>
2026-03-19 17:36:36 +1100

    Initial push of contents from copilot-dev-days/appmod-workshop-java

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+// Sample Java file
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# AppMod Workshop Java
+
+This repository contains the code and resources for the AppMod Workshop using Java.
